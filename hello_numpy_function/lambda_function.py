e73606ae058605aea67b80f38383991abe2b4adb
Wiphoo Methachawalit <wiphoo.m@gmail.com>
2021-07-07 18:26:20 +0700

    example bundle specific numpy version to the package to deploy on aws lambda.

---
+++ hello_numpy_function/lambda_function.py
@@ -0,0 +1,9 @@
+import json
+import numpy
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
